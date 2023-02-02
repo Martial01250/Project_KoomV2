ce7abe37db3f0ee5246a06616698eaf6069e4883
martial01250 <m.trepot@live.fr>
2023-02-02 11:58:51 +0100

    project initialization create user create security

---
+++ assets/app.js
@@ -0,0 +1,37 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+
+// start the Stimulus application
+import './bootstrap';
+
+// require('@fortawesome/fontawesome-free/css/all.min.css')
+// require('@fortawesome/fontawesome-free/js/all.js')
+
+const hamburgerToggler = document.querySelector(".hamburger");
+const navLinksContainer = document.querySelector(".navlinks-container");
+
+const toggleNav = () => {
+    hamburgerToggler.classList.toggle("open");
+
+    const ariaToggle = hamburgerToggler.getAttribute("aria-expanded") === "true" ?  "false" : "true";
+    hamburgerToggler.setAttribute("aria-expanded", ariaToggle)
+
+    navLinksContainer.classList.toggle("open");
+}
+hamburgerToggler.addEventListener("click", toggleNav)
+
+new ResizeObserver(entries => {
+    if(entries[0].contentRect.width <= 920){
+        navLinksContainer.style.transition = "transform 0.3s ease-out"
+    } else {
+        navLinksContainer.style.transition = "none"
+    }
+}).observe(document.body)
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', TextType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'Entrez votre Email'
+                ],
+                'constraints' => [
+                    new Email(),
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'label' => 'Password',
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mot de passe ne correspondent pas',
+                'required' => true,
+
+                'first_options'  => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Entrez votre mot de passe'
+                    ],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Entrez votre mot de passe',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Votre mot se passe doit contenir min {{ limit }} characters',
+                            'max' => 50,
+                        ]),
+                    ],
+                ],
+
+                'second_options' => [
+                    'label' => 'Confirmation mot de passe',
+                    'attr' => ['placeholder' => 'Confirmation de votre Mot de passe'],
+                ],
+
+                'mapped' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
