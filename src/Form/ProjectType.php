2cb9d70026e87f195b4536263bf1d4d79d443561
martial01250 <m.trepot@live.fr>
2023-02-14 15:27:05 +0100

    add create project view

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Project;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +13,18 @@ class ProjectType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('created_date')
+            ->add('name', TextType::class, [
+                'label' => 'Nom du projet',
+                'attr' => [
+                    'placeholder' => 'Entrez le nom de votre projet',
+                ],
+            ] )
             ->add('start_date')
             ->add('end_date')
             ->add('nb_employees')
             ->add('short_description')
             ->add('details')
-            ->add('users')
+
         ;
     }
 
